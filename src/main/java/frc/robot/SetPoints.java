dce50f38293e42b3e4aa361b4b0a5b553ee085ed
WelshDrag0n <everythingamabob@gmail.com>
2024-02-17 12:53:01 -0800

    2/17
    wrist PID
    wrist positioning

--- src/main/java/frc/robot/SetPoints.java
+++ src/main/java/frc/robot/SetPoints.java
@@ -11,8 +11,8 @@ public final class SetPoints {
     // Currently using pid control on the spark so this should be in rads to be consistent with
     // other subs
     public static final double kStowed = Units.degreesToRadians(155); // Degrees
-    public static final double kAmpRear = Units.degreesToRadians(180);
-    public static final double kAmpFront = Units.degreesToRadians(160);
+    public static final double kAmpFront = Units.degreesToRadians(180);
+    public static final double kAmpRear = Units.degreesToRadians(160);
     public static final double kTrap = Units.degreesToRadians(190);
     public static final double kSubwoofer = Units.degreesToRadians(155);
     public static final double kIntake = 1.63;
@@ -21,17 +21,17 @@ public final class SetPoints {
   public final class WristSetpoints {
     // Currently using trap profile subsystem so this needs to be in rads
     public static final double kStowed = Units.degreesToRadians(180);
-    public static final double kAmpRear = Units.degreesToRadians(215);
-    public static final double kAmpFront = Units.degreesToRadians(170);
+    public static final double kAmpFront = Units.degreesToRadians(215);
+    public static final double kAmpRear = Units.degreesToRadians(170);
     public static final double kTrap = Units.degreesToRadians(215);
-    public static final double kSubwoofer = Units.degreesToRadians(180);
+    public static final double kSubwoofer = Units.degreesToRadians(155);
     public static final double kIntake = 3.9;
   }
 
   public final class LevetatorSetpoints {
     public static final double kStowed = Units.inchesToMeters(1);
-    public static final double kAmpRear = Units.inchesToMeters(5);
-    public static final double kAmpFront = Units.inchesToMeters(5);
+    public static final double kAmpFront = Units.inchesToMeters(11);
+    public static final double kAmpRear = Units.inchesToMeters(17);
     public static final double kTrap = Units.inchesToMeters(7);
     public static final double kSubwoofer = Units.inchesToMeters(1);
     public static final double kIntake = Units.inchesToMeters(5.5);
